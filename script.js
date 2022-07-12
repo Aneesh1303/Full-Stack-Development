f83492d46ae0e7ba08f96719891b775aa0be5a6d
Aneesh1303 <aneeshkumar1303@gmail.com>
2022-07-12 20:03:09 +0530

    Event Listener

--- script.js
+++ script.js
@@ -1,50 +1,44 @@
-console.log(document)   //document is an object which is bridge betweem javascript and html.
+const button = document.querySelector('button')
+const input = document.querySelector('input#todo-input')
+const list = document.querySelector('ol')
 
-const ele = document.getElementById("temp");
-console.log(ele);
-console.log(typeof ele);     //It will return type of ele which is object.
-console.log(ele.innerHTML)   //innerHTML whill give us the inner html elements.  
-console.log(ele.innerText)   //innerText will only give us the text present.
-
-//Similarly style is an another object through which we can change csss properties of the elements.
-console.log(ele.style)
-ele.style = "background-color: red; color: white"   //It will change the background-color and color property of element with id temp.
-
-//getElementsByClassName()    It will select elements with same class name
-//get ElementsByTagName()     It will select according to a specified tag like <button> <p> etc.
-//querySelector()             It will select elements same way we did in stles.css.
-
-let element = document.getElementsByClassName('temp')   //getElementsByClassName has a html collection to store the elements which does not support various methods like below one.
-//similar for the case in getElementsByTagName both do not support forEach method and other methods as well which are provided by an array.
-// element.forEach((el, i) => {    
-//     el.innerHTML = `Element ${i}`   //it will prduce an error
-// });
-
-let element2 = document.querySelectorAll('div.tmp')  //while in case of querySelector it store elements in node list which does support some of the methods of array
-element2.forEach((el, i) => {    
-    el.innerHTML = `Element ${i}`   //while it doesn't
-});
-
-const body = document.querySelector('body')
-
-const task = ["Coding", "coding", "Web Dev"]
-
-const ol = document.createElement('ol')
-
-task.forEach(el => {
-    const li = document.createElement('li')
-    const textNode = document.createTextNode(el)
-    li.appendChild(textNode)
-    ol.appendChild(li)
+input.addEventListener('keydown', e => {
+    const key = e.key
+    if(key == 'Enter') {
+        addItem()
+    }
 })
 
-body.appendChild(ol)
+const addItem = (e) => {
+    const textToAdd = input.value
+    
+    if(textToAdd != '') {
+        const element = document.createElement('li')
+        const text = document.createTextNode(textToAdd)
 
-//Event Listener
-const button = document.querySelector('button')
+        element.appendChild(text)
+        list.appendChild(element)
 
-const eventListener = () => {
-    console.log('Hi')
+        const button = document.createElement('button')
+        button.innerHTML = "Delete"
+
+        button.addEventListener('click', (e) => {
+            element.remove()
+        })
+        element.appendChild(button)
+
+        input.value = ""
+    }
+    else {
+        window.alert("Task Can't be empty!!!")
+    }
 }
 
-button.addEventListener('click', eventListener)     //Accepts type of event and a fucntion.
+button.addEventListener('click', addItem)
+
+const button1 = document.querySelector('button')
+const div = document.querySelector('div.first')
+const body = document.querySelector('body')
+
+body.classList.add('light')     // To add class to an element
+body.classList.remove('light')  //To remove a class from the element
